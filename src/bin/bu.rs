29dd2c4a038443464a0e8065052ad5e8eb974445
tearne <tearne@gmail.com>
2025-05-17 14:59:40 +0100

    wip

--- src/bin/bu.rs
+++ src/bin/bu.rs
@@ -1,78 +1,122 @@
-use aws_sdk_s3 as s3;
+use aws_sdk_s3::{operation::{list_object_versions::ListObjectVersionsOutput, list_objects_v2::ListObjectsV2Output}, types::{BucketVersioningStatus, Object}, Client};
+use tokio::runtime::{Handle, Runtime};
+use color_eyre::{eyre::OptionExt, Result};
 
-#[::tokio::main]
-async fn main() -> Result<(), s3::Error> {
-    let config = aws_config::load_from_env().await;
-    let client = aws_sdk_s3::Client::new(&config);
+// #[::tokio::main]
+fn main() -> Result<()> {
+    let runtime = Runtime::new().unwrap();
+    let handle = runtime.handle().clone();
 
-    // ... make some calls with the client
+    let client = {
+            let config = handle.block_on(
+                aws_config::load_from_env()
+            );
+            Client::new(&config)
+        };
+
+
+    let s3 = S3Wrapper{
+        handle,
+        client
+    };
 
     Ok(())
 }
 
-pub fn purge_all_versions_of_everything_in_prefix(&self) -> ABCDResult<()> {
-    self.handle.block_on(async {
-        self.assert_versioning_active().await?;
-        let version_pages = self.get_versions(&self.prefix).await?;
+struct S3Wrapper {
+    handle: Handle,
+    client: Client
+}
 
-        for page in version_pages {
-            let mut object_identifiers = Vec::new();
+impl S3Wrapper {
+    async fn list_objects_v2(&self, bucket: &str, prefix: &str) -> Result<Vec<Object>> {
+        let mut acc: Vec<Object> = Vec::new();
 
-            let object_versions = page.versions.unwrap_or_default();
-            let delete_markers = page.delete_markers.unwrap_or_default();
+        async fn next_page(
+            client: &Client,
+            bucket: &str,
+            prefix: &str,
+            c_tok: Option<String>,
+        ) -> Result<ListObjectsV2Output> {
+            client
+                .list_objects_v2()
+                .bucket(bucket)
+                .prefix(prefix)
+                .set_continuation_token(c_tok)
+                .send()
+                .await
+                .map_err(|e| e.into())
+        }
 
-            let it = delete_markers.into_iter().map(|item| {
-                ObjectIdentifier::builder()
-                    .set_version_id(item.version_id)
-                    .set_key(item.key)
-                    .build()
-            });
-            object_identifiers.extend(it);
+        let mut c_token = None;
+        loop {
+            let list_output = next_page(&self.client, bucket, prefix, c_token).await?;
 
-            let it = object_versions.into_iter().map(|item| {
-                ObjectIdentifier::builder()
-                    .set_version_id(item.version_id)
-                    .set_key(item.key)
-                    .build()
-            });
-            object_identifiers.extend(it);
+            c_token = list_output.next_continuation_token().map(str::to_string);
 
-            if !object_identifiers.is_empty() {
-                log::info!("Deleting {} identifiers", object_identifiers.len());
-                self.client
-                    .delete_objects()
-                    .bucket(&self.bucket)
-                    .delete(
-                        Delete::builder()
-                            .set_objects(Some(object_identifiers))
-                            .build(),
-                    )
-                    .send()
-                    .await?;
-            } else {
-                log::info!("Nothing to delete")
+            if let Some(mut items) = list_output.contents {
+                acc.append(&mut items);
+            }
+
+            if c_token.is_none() {
+                break;
             }
         }
 
-        ABCDResult::Ok(())
-    })
-}
+        Ok(acc)
+    }
 
-async fn assert_versioning_active(&self) -> ABCDResult<()> {
-    let enabled = self
-        .client
-        .get_bucket_versioning()
-        .bucket(&self.bucket) 
-        .send()
-        .await?
-        .status
-        .map(|s| s == BucketVersioningStatus::Enabled)
-        .unwrap_or(false);
-    if enabled {
-        Ok(())
-    } else {
-        Err(ABCDErr::InfrastructureError(
-            "S3 bucket versioning must be enabled".into(),
-        ))
+    async fn is_versioning_enabled(&self, bucket: &str) -> Result<bool> {
+        self
+            .client
+            .get_bucket_versioning()
+            .bucket(bucket) 
+            .send()
+            .await?
+            .status
+            .map(|s| s == BucketVersioningStatus::Enabled)
+            .ok_or_eyre("Error during version checking")
+    }
+
+    async fn get_versions(&self, bucket: &str, prefix: &str) -> Result<Vec<ListObjectVersionsOutput>> {
+        async fn next_page(
+            client: &Client,
+            bucket: &str,
+            prefix: &str,
+            next_key: Option<String>,
+            next_version: Option<String>,
+        ) -> Result<ListObjectVersionsOutput> {
+            client
+                .list_object_versions()
+                .bucket(bucket)
+                .prefix(prefix)
+                .set_key_marker(next_key)
+                .set_version_id_marker(next_version)
+                .send()
+                .await
+                .map_err(|e| e.into())
+        }
+
+        let mut next_key = None;
+        let mut next_version = None;
+
+        let mut acc: Vec<ListObjectVersionsOutput> = Vec::new();
+
+        loop {
+            let out = next_page(&self.client, bucket, prefix, next_key, next_version).await?;
+
+            next_key = out.next_key_marker.clone().map(String::from);
+            next_version = out.next_version_id_marker.clone().map(String::from);
+
+            acc.push(out);
+
+            log::debug!("Found {} page(s) of version identifiers.", acc.len());
+
+            if next_key.is_none() && next_version.is_none() {
+                break;
+            }
+        }
+
+        Ok(acc)
     }
 }
