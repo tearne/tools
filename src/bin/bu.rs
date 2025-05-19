4936cd0f5ae69cce00659343f16cb495aac68f4e
Oliver Tearne <oliver.tearne@apha.gov.uk>
2025-05-19 23:05:34 +0100

    WIP

--- src/bin/bu.rs
+++ src/bin/bu.rs
@@ -1,14 +1,32 @@
-use aws_sdk_s3::{operation::{list_object_versions::ListObjectVersionsOutput, list_objects_v2::ListObjectsV2Output}, types::{BucketVersioningStatus, Object}, Client};
+use std::collections::HashSet;
+
+use aws_sdk_s3::{operation::{get_object_attributes::GetObjectAttributesOutput, list_object_versions::ListObjectVersionsOutput, list_objects_v2::ListObjectsV2Output}, types::{BucketVersioningStatus, Delete, Object, ObjectIdentifier, ObjectVersion}, Client};
+use bytesize::ByteSize;
+use clap::Parser;
 use tokio::runtime::{Handle, Runtime};
 use color_eyre::{eyre::OptionExt, Result};
 
-// #[::tokio::main]
+#[derive(Parser)]
+#[command(version, about)]
+/// Run a command, monitoring CPU and RAM usage at regular intervals and saving to a CSV file.
+struct Cli {
+    /// Bucket
+    #[structopt(short, long)]
+    bucket: String,
+
+    /// Prefix
+    #[structopt(short, long, default_value="")]
+    prefix: String,
+}
+
 fn main() -> Result<()> {
+    let cli = Cli::parse();
+
     let runtime = Runtime::new().unwrap();
     let handle = runtime.handle().clone();
 
     let client = {
-            let config = handle.block_on(
+        let config = handle.block_on(
                 aws_config::load_from_env()
             );
             Client::new(&config)
@@ -20,6 +38,8 @@ fn main() -> Result<()> {
         client
     };
 
+    s3.do_stuff(&cli.bucket, &cli.prefix);
+
     Ok(())
 }
 
@@ -29,6 +49,92 @@ struct S3Wrapper {
 }
 
 impl S3Wrapper {
+    pub fn do_stuff(&self,bucket: &str, prefix: &str) -> Result<()> {
+        self.handle.block_on(async {
+            println!("{bucket}/{prefix}");
+
+            let objects = self.list_objects_v2(bucket, prefix).await.unwrap();
+            let size = ByteSize::b(objects.iter().map(|o|o.size.unwrap()).sum::<i64>() as u64);
+            println!(" * {} across {} objects", size, objects.len());
+
+            if self.is_versioning_enabled(bucket).await.unwrap() {
+                let versions = self.get_object_versions(bucket, prefix).await.unwrap();
+                
+                // let count = versions.iter().filter(|t|t.is_latest().unwrap_or(false)).count();
+                let size =  ByteSize::b(versions.iter().map(|t|t.size.unwrap()).sum::<i64>() as u64);
+                println!("Total usage is {} ({} objects) of which:", size, versions.len());
+                
+                let current: Vec<_> = versions.iter().filter(|t|{
+                    t.is_latest.unwrap_or(false)
+                }).collect();
+                let current_object_keys: HashSet<String> = current.iter().map(|t|{
+                    t.key.as_ref().unwrap().clone()
+                }).collect();
+                let size = 
+                ByteSize::b(current.iter().map(|o|o.size.unwrap()).sum::<i64>() as u64); 
+                println!(" * {} is current ({} objects)", size, current_object_keys.len());
+
+
+                println!(" * ?? are old versions of current objects (?? versions across ?? objects)");
+
+
+                let orphans: Vec<_> = versions.iter().filter(|t|{
+                    let is_current_version = t.is_latest.unwrap_or(false);
+                    let is_orphan = !t.key().map(|k|current_object_keys.contains(k)).unwrap_or(false);
+                    !is_current_version && is_orphan
+                }).collect();
+                let orphan_keys_unique = orphans.iter().map(|t|t.key().unwrap()).collect::<HashSet<_>>().len();
+                let size = ByteSize::b(orphans.iter().map(|o|o.size.unwrap()).sum::<i64>() as u64); 
+                println!(" * {} are versions of deleted objects ({} versions across {} objects)", size, orphans.len(), orphan_keys_unique);
+            } else {
+                println!(" * Versioning is NOT active");
+                let objects = self.list_objects_v2(bucket, prefix).await.unwrap();
+                let size = ByteSize::b(objects.iter().map(|o|o.size.unwrap()).sum::<i64>() as u64);
+                println!(" * {} across {} objects", size, objects.len());
+            }
+            
+
+            
+        });
+        
+        Ok(())
+    }
+
+    async fn get_object_versions(&self, bucket: &str, prefix: &str) -> Result<Vec<ObjectVersion>> {
+        let pages = self.get_versions(bucket, prefix).await.unwrap();
+        let object_versions: Vec<ObjectVersion> = pages.into_iter()
+            .flat_map(|page|
+                page.versions.unwrap_or_default())
+            .collect();
+
+
+        // let mut handles = Vec::new();
+    
+        // for object_identifier in object_identifiers {
+        //     let obj_identifier = object_identifier.unwrap();
+        //     handles.push(tokio::spawn(self.client
+        //         .get_object_attributes()
+        //         .bucket(bucket)
+        //         .key(obj_identifier.key)
+        //         .version_id(obj_identifier.version_id.unwrap())
+        //         .send()));
+        // }
+
+        // //TODO replace with tokio::task::JoinSet
+        // let mut out = Vec::new();
+        // for handle in handles {
+        //     out.push(handle.await.unwrap())
+        // }
+
+        // let t: Result<Vec<GetObjectAttributesOutput>,_> = out.into_iter().collect();
+        
+        // let t: Vec<_> = t.unwrap().iter().map(|t|{
+        //     t.object_size.unwrap()
+        // }).collect();
+
+        Ok(object_versions)
+    }
+
     async fn list_objects_v2(&self, bucket: &str, prefix: &str) -> Result<Vec<Object>> {
         let mut acc: Vec<Object> = Vec::new();
 
