ea1fe2278650208ddd388bd52c41fa7a8fff1e00
nick-pestell <nick@njpestell.com>
2025-07-08 18:44:27 +0100

    wip

--- src/gpu.rs
+++ src/gpu.rs
@@ -1,5 +1,8 @@
+use env_logger::fmt::Timestamp;
 use nvml_wrapper::{Nvml, error::NvmlError, struct_wrappers::device::ProcessUtilizationSample};
 use sysinfo::{Pid, System, ThreadKind};
+
+use crate::gpu;
 
 pub struct Gpu {
     nvml: Nvml,
@@ -19,38 +22,54 @@ impl Gpu {
     pub fn get_all_gpu_utilisation<T>(
         &self,
         last_seen_timestamp: T,
-    ) -> (u64, Vec<Vec<ProcessUtilizationSample>>)
+        device_id: Option<u32>,
+    ) -> Vec<Vec<ProcessUtilizationSample>>
     where
         T: Into<Option<u64>>,
         T: Clone,
     {
-        let num_devices = self.nvml.device_count().unwrap();
-        println!("You have {} GPU devices", num_devices);
         let mut all_utilisation = Vec::new();
-        for idx in 0..num_devices {
-            let device = self.nvml.device_by_index(idx).unwrap();
-            all_utilisation.push(
-                match device.process_utilization_stats(last_seen_timestamp.clone()) {
-                    Ok(result) => result,
-                    Err(e) => match e {
-                        NvmlError::Uninitialized => panic!("{e}"),
-                        _ => {
-                            println!("{e}");
-                            continue;
-                        }
-                    },
-                },
-            )
+        match device_id {
+            Some(device_id) => {
+                let device = self.nvml.device_by_index(device_id).unwrap();
+                all_utilisation.push(
+                    device
+                        .process_utilization_stats(last_seen_timestamp.clone())
+                        .unwrap(),
+                )
+            }
+            None => {
+                let num_devices = self.nvml.device_count().unwrap();
+                println!("You have {} GPU devices", num_devices);
+                let mut all_utilisation = Vec::new();
+                for idx in 0..num_devices {
+                    let device = self.nvml.device_by_index(idx).unwrap();
+                    all_utilisation.push(
+                        match device.process_utilization_stats(last_seen_timestamp.clone()) {
+                            Ok(result) => result,
+                            Err(e) => match e {
+                                NvmlError::Uninitialized => panic!("{e}"),
+                                _ => {
+                                    println!("{e}");
+                                    continue;
+                                }
+                            },
+                        },
+                    )
+                }
+            }
         }
         println!("{:#?}", all_utilisation);
-        return (all_utilisation[0][0].timestamp, all_utilisation);
+        return all_utilisation;
     }
 
     pub fn get_process_utilisation(
         &self,
         process_pid: u32,
         all_gpu_utilisation: &Vec<Vec<ProcessUtilizationSample>>,
-    ) -> u32 {
+        device_id: Option<u32>,
+        timestamp: Option<u64>,
+    ) -> (u32, u64, u32) {
         let children = Self::get_children(process_pid);
         log::trace!(
             "Process {} has Children {:?}",
@@ -59,30 +78,29 @@ impl Gpu {
         );
         let child_utilisation: u32 = Self::get_children(process_pid)
             .into_iter()
-            .map(|child| Self::get_process_utilisation(&self, child, all_gpu_utilisation))
-            .sum();
-
-        let process_utilisation: u32 = all_gpu_utilisation
-            .iter()
-            .flat_map(|process_util| {
-                process_util.iter().map(|util| {
-                    if process_pid == util.pid {
-                        util.sm_util
-                    } else {
-                        0
-                    }
-                })
+            .map(|child| {
+                Self::get_process_utilisation(
+                    &self,
+                    child,
+                    all_gpu_utilisation,
+                    device_id,
+                    timestamp,
+                )
+                .2
             })
             .sum();
 
-        log::trace!(
-            "Process {} has child load {:?}",
-            process_pid,
-            child_utilisation
-        );
-        log::trace!("Process {} has load {:?}", process_pid, process_utilisation);
-
-        process_utilisation + child_utilisation
+        let mut process_utilisation: u32 = 0;
+        for device_id in 0..all_gpu_utilisation.size_of() {
+            for gpu_process in all_gpu_utilisation[device_id] {
+                if process_pid == gpu_process.pid {
+                    let device_id = device_id;
+                    let timestamp = gpu_process.timestamp;
+                    let process_utilisation = gpu_process.sm_util;
+                }
+            }
+        }
+        return (device_id, timestamp, process_utilisation + child_utilisation)
     }
 
     fn get_children(process_id: u32) -> Vec<u32> {
