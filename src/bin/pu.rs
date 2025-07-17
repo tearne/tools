097fc4f1937e9bb5df78b5b2e93458969c72dea2
Oliver Tearne <oliver.tearne@apha.gov.uk>
2025-07-17 06:06:35 +0100

    Root process quits when using separate thread?!

--- src/bin/pu.rs
+++ src/bin/pu.rs
@@ -1,4 +1,4 @@
-use std::{fs::canonicalize, path::Path, process::Command};
+use std::{fs::canonicalize, path::Path, process::Command, sync::{Arc, Mutex}};
 use chrono::{DateTime, Local};
 use clap::Parser;
 use sysinfo::{ProcessRefreshKind, ProcessesToUpdate, System};
@@ -33,62 +33,73 @@ fn main() -> Result<()> {
     let cli = Cli::parse();
     setup_logging(cli.verbose);
 
-    let mut writer = canonicalize(Path::new(&cli.file))
+    let writer = canonicalize(Path::new(&cli.file))
         .wrap_err("failed to canonicalize path")
         .and_then(|abs_path|{
             csv::Writer::from_path(&abs_path)
                 .wrap_err("Failed to create output file writer")
         })?;
 
-    let mut process = Command::new(&cli.command[0])
+    let writer = Arc::new(Mutex::new(writer));
+
+    let mut command_process = Command::new(&cli.command[0])
         .args(&cli.command[1..])
         .spawn()
         .expect("Command failed to start.");
 
-    let pid = process.id();
+    let pid = command_process.id();
     log::trace!("Started pid {}", pid);
     let pause = std::time::Duration::from_secs(cli.interval);
     let start_time = Local::now();
+    let writer_cloned = writer.clone();
 
-    let mut sys = System::new_all();
-    
-    let system_memory = sys.total_memory() as f32;
+    let thread = std::thread::spawn(move ||{
+        let mut sys = System::new_all();
+        
+        let system_memory = sys.total_memory() as f32;
+        let mut wrt_guard = writer_cloned.lock().unwrap();
+        
+        loop{
+            std::thread::sleep(pause);
 
-    std::mem::drop(process);
+            sys.refresh_processes_specifics(
+                ProcessesToUpdate::All,
+                true,
+                ProcessRefreshKind::nothing()
+                    .with_memory()
+                    .with_cpu()
+                    .with_tasks()
+            );
 
-    loop{
-        std::thread::sleep(pause);
+            // match process.try_wait() {
+            //     Ok(Some(status)) => {
+            //         log::info!("Command exited with status {:?}", status);
+            //         break;
+            //     },
+            //     Ok(None) => (),
+            //     Err(_) => bail!("Failed to query status of spawned command"),
+            // }
 
-        sys.refresh_processes_specifics(
-            ProcessesToUpdate::All,
-            true,
-            ProcessRefreshKind::nothing()
-                .with_memory()
-                .with_cpu()
-                .with_tasks()
-        );
+            if !pid_is_alive(pid, &sys) {
+                log::info!("pid {} is dead", pid);
+                break;
+            }
 
-        // match process.try_wait() {
-        //     Ok(Some(status)) => {
-        //         log::info!("Command exited with status {:?}", status);
-        //         break;
-        //     },
-        //     Ok(None) => (),
-        //     Err(_) => bail!("Failed to query status of spawned command"),
-        // }
+            let cpu_ram = get_pid_utilisation(pid, &mut sys);
 
-        if !pid_is_alive(pid, &sys) {
-            log::info!("pid {} is dead", pid);
-            break;
+            let record = UsageRecord::new(start_time, cpu_ram, system_memory);
+            // let writer = wrt_guard.as_mut().unwrap();
+            wrt_guard.serialize(record).unwrap();
+            wrt_guard.flush().unwrap();
         }
+    });
 
-        let cpu_ram = get_pid_utilisation(pid, &mut sys);
-
-        let record = UsageRecord::new(start_time, cpu_ram, system_memory);
-        writer.serialize(record).unwrap();
-        writer.flush().unwrap();
-    }
-
+    log::info!("Waiting for command to complete...");
+    command_process.wait()?;
+    log::info!("Waiting for monitoring thread...");
+    thread.join().unwrap();
+    log::info!("Flushing report...");
+    writer.lock().unwrap().flush()?;
     log::info!("Usage report written to {}", &cli.file);
 
     Ok(())
