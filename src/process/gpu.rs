289906b2a6212dabf0e9b262d689a4374f995a42
nick-pestell <nick@njpestell.com>
2025-07-31 08:54:44 +0100

    towards waiting for process to start

--- src/process/gpu.rs
+++ src/process/gpu.rs
@@ -1,16 +1,21 @@
 use std::{process::Command, str::from_utf8};
 
-use nvml_wrapper::{struct_wrappers::device::ProcessUtilizationSample, Device, Nvml};
-use color_eyre::{eyre::{bail, Context, ContextCompat}, Result};
+use color_eyre::{
+    Result,
+    eyre::{Context, ContextCompat, bail},
+};
+use nvml_wrapper::{
+    Device, Nvml, error::NvmlError, struct_wrappers::device::ProcessUtilizationSample,
+};
 use sysinfo::Pid;
 
 use crate::process::system::System;
 
 pub struct GpuDevices<'a>(Vec<Device<'a>>);
 
-pub struct Usage{
+pub struct Usage {
     pub percent: u32,
-    pub last_seen_timestamp: u64
+    pub last_seen_timestamp: u64,
 }
 
 pub struct GpuApi {
@@ -27,17 +32,23 @@ impl GpuApi {
             bail!("`lspci` did not confirm the presence of a GPU")
         }
 
-        Ok(Self { nvml: Nvml::init()? })
+        Ok(Self {
+            nvml: Nvml::init()?,
+        })
     }
 
     pub fn build_devices<'a>(&'a self) -> Result<GpuDevices<'a>> {
         let num_devices = self.nvml.device_count()?;
         let devices = (0..num_devices)
-            .map(|idx|self.nvml.device_by_index(idx).wrap_err("Device initialisation failure"))
+            .map(|idx| {
+                self.nvml
+                    .device_by_index(idx)
+                    .wrap_err("Device initialisation failure")
+            })
             .collect::<Result<Vec<Device<'a>>>>()?;
 
         log::debug!("Found devices: {:?}", &devices);
-        
+
         Ok(GpuDevices(devices))
     }
 
@@ -45,18 +56,28 @@ impl GpuApi {
         &self,
         devices: &GpuDevices,
         last_seen_timestamp: Option<u64>,
-    ) -> Result<Vec<ProcessUtilizationSample>>
-    {
-        let stats: Result<Vec<ProcessUtilizationSample>> = devices.0
+    ) -> Result<Vec<ProcessUtilizationSample>> {
+        let stats: Result<Vec<ProcessUtilizationSample>> = devices
+            .0
             .iter()
-            .map(|d|{
-                d.process_utilization_stats(last_seen_timestamp)
-                    .wrap_err("Failed to get device utilisation sample")
+            .map(|d| {
+                loop {
+                    match d.process_utilization_stats(last_seen_timestamp) {
+                        Err(e) => match e {
+                            NvmlError::NotFound => continue,
+                            _ => return Err(e).wrap_err("Failed to get device utilisation sample"),
+                        },
+                        Ok(result) => return Ok(result),
+                    }
+                }
             })
-            .try_fold(Vec::<ProcessUtilizationSample>::new(), |mut acc, res_samples| -> Result<_>{
-                acc.extend(res_samples?);
-                Result::Ok(acc)
-            });
+            .try_fold(
+                Vec::<ProcessUtilizationSample>::new(),
+                |mut acc, res_samples| -> Result<_> {
+                    acc.extend(res_samples?);
+                    Result::Ok(acc)
+                },
+            );
 
         stats
     }
@@ -66,35 +87,31 @@ impl GpuApi {
         devices: &GpuDevices,
         pid: Pid,
         last_seen_timestamp: Option<u64>,
-        system: &mut System
+        system: &mut System,
     ) -> Result<Usage> {
         let children = system.get_pid_tree(pid, false);
-        log::trace!(
-            "Process {} has Children {:?}",
-            pid,
-            children
-        );
+        log::trace!("Process {} has Children {:?}", pid, children);
 
         let all_utilisation = self.get_all_utilisation(devices, last_seen_timestamp)?;
-        let max_timestamp: u64 = all_utilisation.iter()
+        let max_timestamp: u64 = all_utilisation
+            .iter()
             .max_by_key(|sample| sample.timestamp)
             .map(|sample| sample.timestamp)
             .wrap_err("Failed to identify max timestamp from GPU process utilisation data.")?;
 
         let sum = all_utilisation
             .iter()
-            .filter_map(|p_sample|
-                match children.contains(&Pid::from_u32(p_sample.pid)) {
+            .filter_map(
+                |p_sample| match children.contains(&Pid::from_u32(p_sample.pid)) {
                     true => Some(p_sample.sm_util),
                     false => None,
-                }
+                },
             )
             .sum();
 
-        Ok(Usage { 
-            percent: sum, 
-            last_seen_timestamp: max_timestamp
+        Ok(Usage {
+            percent: sum,
+            last_seen_timestamp: max_timestamp,
         })
     }
-
 }
